fd6bb6d91f16ad7720d72e2e7d24b984b79675f5
unknown <aida.pirusic@hotmail.com>
2019-09-25 15:36:32 +0200

    Todo component: template and style.

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <p>Item 1</p>
+</template>
+
+<script>
+export default {
+    name:"TodoItem",
+    props:["todoItem"],
+    methods:{
+        
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
