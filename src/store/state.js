a23047836f7ffc767564f8e260031c18294eb0c4
unknown <aida.pirusic@hotmail.com>
2019-09-30 20:09:04 +0200

    Vuex fixes.

--- src/store/state.js
+++ src/store/state.js
@@ -1,16 +1,6 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-
-import state from './state';
-import getters from './getters';
-import mutations from './mutations';
-import actions from './actions';
-
-Vue.use(Vuex);
-
-export default new Vuex.Store({
-  state,
-  getters,
-  mutations,
-  actions,
-});
+export default {
+    todoList: JSON.parse(localStorage.getItem('todoList')),
+    doneList: JSON.parse(localStorage.getItem('doneList')),
+    loggedUser: JSON.parse(localStorage.getItem('loggedUser')),
+  };
+  
