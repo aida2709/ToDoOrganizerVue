c89d6bd4220e3dc2f91566eaf8edf4ab23fda795
unknown <aida.pirusic@hotmail.com>
2019-09-30 19:55:21 +0200

    services added.

---
+++ src/services/AuthService.js
@@ -0,0 +1,51 @@
+export default {
+
+    login(Email, password, RememberMe) {
+        if (Email === "test@test.com" && password === "test") {
+            let user = {
+                Email,
+                Username: "Username",
+                RememberMe,
+            }
+
+            localStorage.setItem('loggedUser', JSON.stringify(user));
+
+            return user;
+        }
+        return null;
+    },
+
+    isLogged() {
+        let data = localStorage.getItem('loggedUser');
+
+        if (data) {
+            let user = JSON.parse(data);
+            if(user)
+                return true;
+
+            return false;
+        }
+        return false;
+    },
+
+    isRememberMeActivated() {
+        let data = localStorage.getItem('loggedUser');
+
+        if (data) {
+            let user = JSON.parse(data);
+            return (user && user.RememberMe);
+        }
+        return false;
+    },
+
+    logout() {
+        localStorage.removeItem('loggedUser');
+        //localStorage.clear();
+    },
+
+    getUsername() {
+        let user = JSON.parse(localStorage.getItem('loggedUser'));
+
+        return user ? user.Username : '';
+    }
+}
